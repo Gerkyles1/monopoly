38b3424926ed7ee381a90f912f9f351603f7e768
Gerkyles1 <maxfast333@gmail.com>
2023-05-28 14:22:46 +0300

    synchronized player animations

--- Monopoly Fortune/Assets/scripts/playerMoving.cs
+++ Monopoly Fortune/Assets/scripts/playerMoving.cs
@@ -1,23 +1,30 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class playerMoving : MonoBehaviour
 {
     private Animator animator;
+    public float animationDelay = 0.5f; // Задержка между анимациями
+
     void Start()
     {
         animator = GetComponent<Animator>();
     }
+
     public void step(int count)
     {
-        for (int i=0;i<count;i++)
-        {
-            animator.SetTrigger("jump");
+        StartCoroutine(PlayAnimations(count));
+    }
 
+    private IEnumerator PlayAnimations(int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            animator.applyRootMotion = true;
+            animator.SetTrigger("jump");
+            yield return new WaitForSeconds(animationDelay);
+            animator.applyRootMotion = false;
 
         }
-
-
     }
 }
